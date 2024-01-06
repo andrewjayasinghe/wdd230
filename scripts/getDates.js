40823431d9b803af9cc350e37bb2e55a28d7a339
Andrew Jayasinghe <55458355+andrewjayasinghe@users.noreply.github.com>
2024-01-06 13:42:33 -0800

    main page

---
+++ scripts/getDates.js
@@ -0,0 +1,10 @@
+
+const date = new Date();
+const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+
+const __textYear = document.querySelector("#footer_year")
+const __modifiedDate = document.querySelector("#last_modified_date")
+
+__textYear.textContent = date.getFullYear();
+__modifiedDate.textContent = `${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')}/${date.getFullYear()} ${time}`;
+
