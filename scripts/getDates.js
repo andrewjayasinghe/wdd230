1279a0a65b44c699e488a6ba696472cc9e57d6ac
Andrew Jayasinghe <55458355+andrewjayasinghe@users.noreply.github.com>
2024-01-10 23:51:27 -0800

    2

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -8,3 +8,10 @@ const __modifiedDate = document.querySelector("#last_modified_date")
 __textYear.textContent = date.getFullYear();
 __modifiedDate.textContent = `${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')}/${date.getFullYear()} ${time}`;
 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
