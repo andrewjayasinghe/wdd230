ce29e6d4ea8e623bbdcb12d27265346775c9b8ff
Andrew Jayasinghe <55458355+andrewjayasinghe@users.noreply.github.com>
2024-01-15 23:32:51 -0800

    3

---
+++ week03/scripts/script.js
@@ -0,0 +1,84 @@
+
+const __images = document.querySelectorAll("img[data-src]");
+
+//This function allows us to use an attribute to switch our place holder
+//and enables us to remove that attribute, which allows us to do some CSS magic.
+function loadImages(image){
+    const newSrc = image.getAttribute("data-src");
+    //If our targeted item does not have a data-src attribute, escape!
+    if(!newSrc){
+        return;
+    }
+    //This is just replacing the placeholder with the new src.
+    image.setAttribute("src", newSrc);
+    //This is telling us to get rid of the attribute after the image loads.
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    }
+}
+
+//Options is defining our view area as we scroll
+let options = {
+    root: document.querySelector("#scrollArea"),
+    rootMargin: "0px",
+    //1 means closer to being centered in screen, 0 means the bottom.
+    threshold: 0,
+};
+
+function callback(entries, imgObserver) {
+    //Entry in this case is an instance of our observer detecting something.
+    entries.forEach((entry) => {
+        //If the image isn't being viewed or isn't viewable, exit out!
+        if(!entry.isIntersecting){
+            return;
+        //If the image is viewable (our entry's target), load the image and stop observing it.
+        } else {
+        loadImages(entry.target);
+        imgObserver.unobserve(entry.target);
+        }
+    })
+}
+
+//This is telling us to call a function, which automatically passes the event causing it, and the
+//object which called it. Then options is our viewable scroll area.
+const observer = new IntersectionObserver(callback, options);
+
+//Here we are telling our new observer to observe every image on the page
+__images.forEach((img) => {
+    observer.observe(img);
+});
+
+const __button = document.querySelector("button");
+const __input = document.querySelector("#favchap");
+const __list = document.querySelector("#list");
+
+let item;
+let button;
+
+__button.addEventListener("click", () => {
+    if(/[a-z]/i.test(__input.value) == false || /[0-9]/.test(__input.value) == false){
+        alert("Not a valid Chapter format")
+        __input.focus();
+        return
+    }
+
+    item = document.createElement("li")
+    button = document.createElement("button")
+
+    button.textContent = "❌";
+    item.textContent = __input.value;
+
+    item.appendChild(button);
+    
+    button.addEventListener("click", (e) => {
+        removeEventListener("click", e.target)
+        e.target.parentElement.remove();
+        __input.value = '';
+        __input.focus();
+    });
+
+    __list.appendChild(item);
+    __input.value = '';
+    __input.focus();
+
+});
