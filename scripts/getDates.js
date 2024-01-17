6ae7817c216ed6cbf25c832377ddb4c6a35e486e
Andrew Jayasinghe <55458355+andrewjayasinghe@users.noreply.github.com>
2024-01-16 20:04:04 -0800

    js

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -33,3 +33,14 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "🕶️";
 	}
 });
+
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
